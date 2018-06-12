227099228b9e24d2a2d715719d49f2078e6ca945
root <root@09CPU0241L.amritavidya.edu>
2018-06-12 14:44:25 +0530

    Mohit

--- website/userauth/views.py
+++ website/userauth/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from . import forms
+from . import models
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 # Create your views here.
 
-def register(request):
+def signup(request):
 	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
+		form = forms.Register(request.POST)
 		if form.is_valid():
 			form.save()
-			return HttpResponse("user Saved")
+			models.Otherdetail(
+				user=User.objects.get(username=form.cleaned_data.get('username')),
+				bio=form.cleaned_data.get('bio'),
+				dob=form.cleaned_data.get('date_of_birth')
+				).save()
+			return HttpResponse("user saved")
+		else:
+			return render(request,'userauth/signup.html',{'form' : form})
 	else:
-		form = UserCreationForm()
-	return render(request, 'userauth/register.html',{'form':form})
+		form = forms.Register()
+	return render(request,'userauth/signup.html',{'form' : form})
--- website/website/urls.py
+++ website/website/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-    url('movie/', include('movie.urls')),
+    # url('music/', include('music.urls')),
     url('auth/',include('userauth.urls')),
 ]
+#urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
